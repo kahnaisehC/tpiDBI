6604f095619bd7c00c111bf33f1a5337c4aae4e2
Ian Hakanson <gabianhak@gmail.com>
2024-11-29 01:24:23 -0300

    feat: add 3rd query

--- consultas.sql
+++ consultas.sql
@@ -9,7 +9,16 @@ END//
 delimiter ;
 
 delimiter // 
-CREATE PROCEDURE inscribir_jugador(IN nid_equipo INT, IN nid_torneo INT , IN nid_jugador INT )
+
+CREATE PROCEDURE registrar_jugador(IN nnombre VARCHAR(50), IN napellido VARCHAR(50), IN nDNI INT, IN nfecha_nacimiento DATE, IN ntelefono VARCHAR(50), IN ndireccion VARCHAR(50), IN nnro_equipo INT, IN nfoto VARCHAR(255))
+BEGIN 
+  INSERT INTO socio(nombre, apellido, DNI, fecha_nacimiento, telefono, direccion) VALUES(nnombre, napellido, nDNI, nfecha_nacimiento, ntelefono, ndireccion);
+  SET @id_socio = (SELECT MAX(id_socio) FROM socio);
+  INSERT INTO jugador(numero_socio, numero_equipo, foto) VALUES(@id_socio, nnro_equipo, nfoto);
+END//
+
+
+CREATE PROCEDURE inscribir_jugador_a_torneo(IN nid_equipo INT, IN nid_torneo INT , IN nid_jugador INT )
 BEGIN
 
   SET @edad = (
@@ -38,4 +47,81 @@ BEGIN
   END IF;
 END//
 
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE generar_fixture(IN nid_torneo INT, IN nro_ruedas INT)
+BEGIN
+IF nro_ruedas <= 0 THEN
+    SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Numero de ruedas menor a 0';
+ELSE
+    SET @maxiA = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'MAXI' && division = 'A'
+    );
+    SET @maxiB = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'MAXI' && division = 'B'
+    );
+    SET @maxiC = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'MAXI' && division = 'C'
+    );
+    SET @superA = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'SUPER' && division = 'A'
+    );
+    SET @superB = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'SUPER' && division = 'B'
+    );
+    SET @superC = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'SUPER' && division = 'C'
+    );
+    SET @masterA = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'MASTER' && division = 'A'
+    );
+    SET @masterB = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'MASTER' && division = 'B'
+    );
+    SET @masterC = (
+      SELECT id_equipo
+      FROM inscripcion
+      LEFT JOIN equipo ON equipo.numero_equipo inscripcion.numero_equipo
+      WHERE id_torneo = nid_torneo && categoria = 'MASTER' && division = 'C'
+    );
+
+    -- maxiA
+    crear_ruedas: LOOP
+    SET nro_ruedas = nro_ruedas -1;
+    IF nro_ruedas < 0 THEN
+      LEAVE crear_ruedas;
+    END IF;
+
+
+
+    ITERATE nro_ruedas;
+    END LOOP;
+  END IF;
+END//
 delimiter ;
